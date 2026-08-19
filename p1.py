10075442e27f8f27c46bbbe80590ce4207b68e85
PrathamSharma7 <prathamsharma7000@gmail.com>
2026-08-19 20:49:55 +0530

    Delete p1.py

--- p1.py
+++
@@ -1,9 +0,0 @@
-"""
-Write a python program to print the contents of a directory using the 
-os module.
-"""
-import os
-# result = os.listdir()
-# print(result)
-this_directory= os.getcwd()
-print(this_directory)

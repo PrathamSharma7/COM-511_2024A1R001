35b334a2440d9a3169eece4780f169b242a69a4a
PrathamSharma7 <prathamsharma7000@gmail.com>
2026-08-19 13:34:11 +0530

    Add files via upload

---
+++ Task_Aug19/p1.py
@@ -0,0 +1,10 @@
+#Write a python program to perform addition, subtraction, multiplication, division of two numbers, also find quotient
+
+a = int(input("Enter number 1: "))
+b = int(input("Enter number 2: "))
+
+print("Addition:", a+b)
+print("Subraction:", a-b)
+print("Multiplication:", a*b)
+print("Division:", a/b)
+print("Quotient:", a//b)
---
+++ Task_Aug19/p2.py
@@ -0,0 +1,6 @@
+#Write a python program to find remainder when a number is divided by z
+
+a = int(input("Enter 1st number: "))
+z = int(input("Enter 2nd number: "))
+
+print("Remainder:", a%z)

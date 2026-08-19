7eef714d0bc209ccd3932bb4220f07df8527c14e
PrathamSharma7 <prathamsharma7000@gmail.com>
2026-08-19 12:24:58 +0530

    initial commit

---
+++ p1.py
@@ -0,0 +1,9 @@
+"""
+Write a python program to print the contents of a directory using the 
+os module.
+"""
+import os
+# result = os.listdir()
+# print(result)
+this_directory= os.getcwd()
+print(this_directory)

a697f9e86b2b317f341130535e35eea2948d515e
PrathamSharma7 <prathamsharma7000@gmail.com>
2026-08-20 13:06:04 +0530

    Commited

--- Tasks_Aug20/task2.py
+++ Tasks_Aug20/task2.py
@@ -4,6 +4,6 @@ principal = float(input("Enter Principal: "))
 rate_of_interest = float(input("Enter Rate of Interest: "))
 time_in_years = int(input("Enter time in years: "))
 
-simple_interest = principal*rate_of_interest*time_in_years
+simple_interest = principal*rate_of_interest*time_in_years/100
 amount = principal + simple_interest
 print("Simple Interest:", simple_interest, "\nAmount:",amount)

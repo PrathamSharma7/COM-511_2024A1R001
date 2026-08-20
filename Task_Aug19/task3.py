c3054831e1e026b1f2f36fb35f4d8fb3f4a626df
PrathamSharma7 <prathamsharma7000@gmail.com>
2026-08-20 12:06:37 +0530

    Changed names of files

--- Task_Aug19/task3.py
+++ Task_Aug19/task3.py
@@ -1,7 +1,7 @@
 #Check the type of variable assigned using input() function
 a = input(">>> ")
-print("Type:", type(a))
+print("a:", a, "Type:", type(a))
 a = int(a)
-print("Type:", type(a))
+print("a:", a, "Type:", type(a))
 a = float(a)
-print("Type:", type(a))
+print("a:", a, "Type:", type(a))

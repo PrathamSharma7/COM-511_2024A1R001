2e8f686d841afc22c085ece61b67bf4fbe5f6970
PrathamSharma7 <prathamsharma7000@gmail.com>
2026-08-20 13:16:00 +0530

    Commited

--- Tasks_Aug20/task4.py
+++ Tasks_Aug20/task4.py
@@ -1,5 +1,8 @@
-#Write a Python program to ask the user for radius and calculate are and circumference of a circle
+#Write a Python program to ask the user for radius and calculate area and circumference of a circle
 
 radius = float(input("Enter radius: "))
-area = 3.14*radius*radius
+pi = 3.141459
+area = pi*radius*radius
+circumference = 2*pi*radius
 print("Area of circle:", area)
+print("Circumference of circle:", circumference)

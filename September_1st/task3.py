90e40518ac94f37577c444240c1fe1e521359906
PrathamSharma7 <prathamsharma7000@gmail.com>
2026-09-01 12:29:11 +0530

    Added programs for september 1st

--- September_1st/task3.py
+++ September_1st/task3.py
@@ -1,7 +1,4 @@
 # Write a Python program to detect double space in a string
 
 string = input("Enter a string: ")
-if "  " in string:
-    print("Double space present.")
-else:
-    print("Double space not present.")
+print(string.find("  "))
--- September_1st/task5.py
+++ September_1st/task5.py
@@ -1,2 +1,5 @@
 # Write a python program to format the following letter using escape sequence characters.
-# letter = "Dear Saurabh, this python course in nice"
+# letter = "Dear Saurabh, this python course in nice, Thanks!"
+
+letter = "Dear Saurabh, \n\tthis python course is nice, \n\tThanks!"
+print(letter)

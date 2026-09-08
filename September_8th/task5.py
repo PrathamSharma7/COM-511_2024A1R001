5e538203596124165b209e59470e8ef082588c22
PrathamSharma7 <prathamsharma7000@gmail.com>
2026-09-08 13:23:10 +0530

    Refactored the logic in task 5 and task 6

--- September_8th/task5.py
+++ September_8th/task5.py
@@ -6,29 +6,11 @@ Marks are considered valid only if they are between 1 and 100. If the marks are 
 the program must display "Invalid Marks Skipped" and move to the next student without printing those marks
 '''
 
-marks_1 = int(input("Enter Marks for student 1: "))
-marks_2 = int(input("Enter Marks for student 2: "))
-marks_3 = int(input("Enter Marks for student 3: "))
-marks_4 = int(input("Enter Marks for student 4: "))
-marks_5 = int(input("Enter Marks for student 5: "))
+for i in range(1,6):
+    marks = int(input("Enter marks: "))
 
-if 0 <= marks_1 <= 100:
-    print("Marks of Student 1:", marks_1)
-else:
-    print("Invalid Marks Skipped")
-if 0 <= marks_2 <= 100:
-    print("Marks of Student 2:", marks_2)
-else:
-    print("Invalid Marks Skipped")
-if 0 <= marks_3 <= 100:
-    print("Marks of Student 3:", marks_3)
-else:
-    print("Invalid Marks Skipped")
-if 0 <= marks_4 <= 100:
-    print("Marks of Student 4:", marks_4)
-else:
-    print("Invalid Marks Skipped")
-if 0 <= marks_5 <= 100:
-    print("Marks of Student 5:", marks_5)
-else:
-    print("Invalid Marks Skipped")
+    if 0 <= marks <= 100:
+        print("Valid Marks:", marks)
+    else:
+        print("Invalid Marks Skipped")
+        
--- September_8th/task6.py
+++ September_8th/task6.py
@@ -1,6 +1,10 @@
 '''
 Write a python program to input four numbers from the user and find the greatest number among them
 '''
+max = float('-inf')
+for i in range(4):
+    num = int(input("Enter number: "))
+    if num > max:
+        max = num
 
-nums = list(map(int, input("Enter 4 Numbers: ").split()))
-print("Greatest out of the four numbers:", max(nums))
+print("Greatest number enetered:", max)

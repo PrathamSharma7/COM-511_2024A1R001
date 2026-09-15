71e3e1c482905fff2e3b560ebb3ab7cac89bc44d
PrathamSharma7 <prathamsharma7000@gmail.com>
2026-09-15 12:14:27 +0530

    Added task 9 and task 10 for september 15th

---
+++ September_15th/task10.py
@@ -0,0 +1,13 @@
+'''
+Write a python program to print a square pattern of stars for n rows and n columns
+****
+****
+****
+****
+'''
+
+n = int(input("Enter number of rows: "))
+for i in range(n):
+    for j in range(n):
+        print("*", end=" ")
+    print()
---
+++ September_15th/task9.py
@@ -0,0 +1,13 @@
+'''
+Write a Python Program to print the following pattern for rows
+1
+1 2
+1 2 3
+1 2 3 4
+'''
+
+rows = int(input("Enter number of rows: "))
+for i in range(1, rows + 1):
+    for j in range(1, i + 1):
+        print(j, end=" ")
+    print()

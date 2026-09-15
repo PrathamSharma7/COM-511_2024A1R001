63c237c48e52a06f9a0b5b80fca188b862d60e92
PrathamSharma7 <prathamsharma7000@gmail.com>
2026-09-15 12:46:10 +0530

    Added task 11 and task 12

---
+++ September_15th/task11.py
@@ -0,0 +1,13 @@
+'''
+Write a python program to print right-angled triangle pattern of stars.
+*
+* * 
+* * *
+* * * *
+'''
+
+n = int(input("Enter number of rows: "))
+for i in range(n):
+    for j in range(i + 1):
+        print("*", end=" ")
+    print()
---
+++ September_15th/task12.py
@@ -0,0 +1,14 @@
+'''
+Write a python program to print an inverted right-angled triangle pattern of stars.
+    * * * * *
+    * * * *
+    * * * 
+    * *
+    *
+'''
+
+n = int(input("Enter number of rows: "))
+for i in range(n, 0, -1):
+    for j in range(i):
+        print("*", end=" ")
+    print()

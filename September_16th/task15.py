2dda42b0bd04e686339e8cfd9c754b8258f5d961
PrathamSharma7 <prathamsharma7000@gmail.com>
2026-09-16 11:58:18 +0530

    Added task 15 for September 16th

---
+++ September_16th/task15.py
@@ -0,0 +1,17 @@
+'''
+Write a Python program to print a hollow square using stars
+* * * * *
+*       *
+*       *
+*       *
+* * * * *
+'''
+
+n = int(input("Enter number of rows and columns: "))
+for i in range(n):
+    for j in range(n):
+        if i == 0 or i == n-1 or j == 0 or j == n-1:
+            print("*", end="  ")
+        else:
+            print(" ", end="  ")
+    print()

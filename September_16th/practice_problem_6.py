18152c85b6dd3385f3debd400547a037a6494cb8
PrathamSharma7 <prathamsharma7000@gmail.com>
2026-09-17 12:38:48 +0530

    Refactored the logic in practice_problem_6

--- September_16th/practice_problem_6.py
+++ September_16th/practice_problem_6.py
@@ -3,8 +3,6 @@ Write a python program to rotate a list one position to the right
 '''
 
 nums = list(map(int, input("Enter a list: ").split()))
-last_element = nums[-1]
-nums.insert(0, last_element)
-nums.pop()
+nums.insert(0, nums.pop())
 
 print(f"List after rotation: {nums}")
